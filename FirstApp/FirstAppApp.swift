edd93acf6854eccf603274689eb98c827f8b3c7a
protrek <leehyungrok@gmail.com>
2022-01-04 21:12:32 +0900

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Lee Hyeongrok on 2022/01/04.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
